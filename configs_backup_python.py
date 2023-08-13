d7a46aded195a83e4b97d72c40f3d6090aff9c93
Ilya Yarusov <originalyia@gmail.com>
2023-08-13 18:59:41 +0400

    refactoring code and add new functional

--- configs_backup_python.py
+++ configs_backup_python.py
@@ -1,105 +1,111 @@
 #!/usr/bin/env python3
 
-# backup всех важных конфигов системы
+# Backup всех важных файлов системы.
 
-import os
 import datetime
+import json
+import os
 import shutil
+from pathlib import Path
+
+import click
+from loguru import logger
+
+from config import config
+
+logger.add("debug.log")
 
 
-def get_current_date_time():
-    separator = "-----------------------"
+def create_directory_save(path_save: Path) -> Path:
     current_date = datetime.datetime.now()
-    current_date_short = current_date.strftime("%d/%m/%Y, %H:%M:%S")
-    header_log = separator + current_date_short + separator
-    return header_log
+    current_date_short = current_date.strftime("%d_%m_%Y_%H_%M_%S")
+    full_name_path_directory = path_save / Path(current_date_short)
+    os.mkdir(str(full_name_path_directory))
+    return full_name_path_directory
 
 
-list_configs = {
-    "zsh": "/home/oriyia/.zshrc",
-    "alacritty": "/home/oriyia/.config/alacritty/",
-    "i3": "/home/oriyia/.config/i3/config",
-    "betterlockscreen": "/home/oriyia/.config/betterlockscreenrc",
-    "nvim": "/home/oriyia/.config/nvim/",
-    "bash_history": "/home/oriyia/.bash_history",
-    "bashrc": "/home/oriyia/.bashrc",
-    "bash_profile": "/home/oriyia/.profile",
-    "python_history": "/home/oriyia/.python_history",
-    "xinitrc": "/home/oriyia/.xinitrc",
-    "xmodmap": "/home/oriyia/.Xmodmap",
-    "xprofile": "/home/oriyia/.xprofile",
-    "xresources": "/home/oriyia/.Xresources",
-    "polybar": "/home/oriyia/.config/polybar/",
-    "ranger": "/home/oriyia/.config/ranger/",
-    "pygments": "/usr/lib/python3/dist-packages/pygments/styles/my.py",
-    "zathura": "/home/oriyia/.config/zathura/zathurarc",
-    "ppa": "/etc/apt/sources.list",
-    "rofi": "/home/oriyia/.config/rofi/config.rasi",
-    "rofi_theme": "/home/oriyia/.local/share/rofi/themes/rofi_mytheme_onedark.rasi",
-    "zsh_history": "/home/oriyia/.zsh_history",
-    "zshrc": "/home/oriyia/.zshrc",
-    "starship": "/home/oriyia/.config/starship.toml",
-    "lsd": "/home/oriyia/.config/lsd/config.yaml",
-    "lsd_themes": "/home/oriyia/.config/lsd/themes/lsd_theme.yaml",
-    "vivid": "/home/oriyia/.config/vivid/vivid_mytheme_onedark.yml",
-    "glow": "/home/oriyia/.config/glow/",
-    "glamour": "/home/oriyia/.config/glow/myconf.json",
-    "ipython_config": "/home/oriyia/.ipython/profile_default/ipython_config.py",
-    "autojump": "/home/oriyia/.local/share/autojump/autojump.txt",
-    "bat_config": "/home/oriyia/.config/bat/config",
-    "bat_theme": "/home/oriyia/.config/bat/themes/sublime-snazzy/NewBatThemeLight.tmTheme",
-    "systemd_yandex_disk": "/etc/systemd/system/yandex-disk.service",
-    "yandex_config": "/home/oriyia/.config/yandex-disk/config.cfg",
-    "delta_themes": "/home/oriyia/.config/themes.gitconfig",
-    "spotifyd": "/home/oriyia/.config/spotifyd",
-    "gitconfig": "/home/oriyia/.gitconfig",
-    "tmux": "/home/oriyia/.tmux.conf.local",
-}
-
-log_file = open("results.txt", "a")
+def create_backup_copy_file_list():
+    path_list_file = os.path.dirname(os.path.abspath(__file__))
+    old_name_file = "backup_files.json"
+    new_name_file = str(Path(old_name_file).stem) + "_bc.json"
+    source = Path(path_list_file) / Path(old_name_file)
+    target = Path(path_list_file) / Path(new_name_file)
+    shutil.copyfile(str(source), str(target))
 
 
-def create_directory(target_path):
-    if not os.path.exists(target_path):
-        os.mkdir(target_path)
-
-
-def write_info_log_file(log_message):
-    log_message = log_message + "\n"
-    log_file.write(log_message)
-
-
-def backup_files(key, path_object):
-    target_path = os.environ["PWD"] + "/saved_configs/" + key + "/"
-    create_directory(target_path)
-    if os.path.isdir(path_object):
-        try:
-            log_message = '+++++ ' + key
-            shutil.copytree(path_object, target_path,
-                            copy_function=shutil.copy2, dirs_exist_ok=True)
-            write_info_log_file(log_message)
-        except BaseException:
-            log_message = '---- ' + key
-            write_info_log_file(log_message)
+def add_new_file_to_list(new_file: str) -> int:
+    path_new_file = Path(new_file)
+    name_new_file = path_new_file.name
+    full_path_new_file = Path.cwd() / path_new_file
+    create_backup_copy_file_list()
+    if not full_path_new_file.exists():
+        click.echo(f"Такого файла не существует: {str(full_path_new_file)}")
+        return 1
     else:
-        try:
-            log_message = '+++++ ' + key
-            shutil.copy2(path_object, target_path)
-            write_info_log_file(log_message)
-        except BaseException:
-            log_message = '---------- ' + key
-            write_info_log_file(log_message)
+        with open("backup_files.json") as file:
+            list_object = json.load(file)
+        while True:
+            name_directory = input("Имя директории: ")
+            if name_directory in list_object:
+                click.echo(f"Директория '{name_directory}' уже есть. Укажите другое.")
+                continue
+            list_object[str(name_new_file)] = str(full_path_new_file)
+            with open("backup_files.json", "w") as json_file:
+                json.dump(list_object, json_file)
+            click.echo(f"Новая запись: '{name_directory}':'{str(full_path_new_file)}'")
+            return 0
 
 
-current_date = get_current_date_time()
-write_info_log_file(current_date)
+@logger.catch
+def copy_files(files: dict, full_name_path_dir: Path):
+    for key, value_dict in files.items():
+        current_directory = Path(full_name_path_dir)
+        new_current_directory = Path(current_directory, key)
+        new_current_directory.mkdir()
+        if type(value_dict) == str:
+            name_file = Path(value_dict).name
+            target = new_current_directory / name_file
+            try:
+                if Path(value_dict).is_dir():
+                    shutil.copytree(value_dict, str(target))
+                else:
+                    shutil.copy2(value_dict, str(target))
+            except FileNotFoundError as FN:
+                logger.debug(f"Файл не удалось скопировать: {value_dict}")
+                logger.debug(f"Error: {FN}")
+        elif type(value_dict) == list:
+            for value_list in value_dict:
+                if type(value_list) == str:
+                    name_file = Path(value_list).name
+                    target = new_current_directory / name_file
+                    try:
+                        if Path(value_list).is_dir():
+                            shutil.copytree(value_list, str(target))
+                        else:
+                            shutil.copy2(value_list, target)
+                    except FileNotFoundError as FN:
+                        logger.debug(f"Файл не удалось скопировать: {value_dict}")
+                        logger.debug(f"Error: {FN}")
+                elif type(value_list) == dict:
+                    copy_files(value_list, new_current_directory)
 
-configs_directory = "saved_configs"
-create_directory(configs_directory)
+
+@click.command()
+@click.option(
+    "-nf", "--new-file", default=None, help="Добавить в список новый сохраняемый файл"
+)
+def backup_files(new_file):
+    """Function save important files"""
+    logger.info("НАЧАЛО ОТЛАДКИ")
+    if new_file:
+        result = add_new_file_to_list(new_file)
+        if result != 0:
+            return
+    full_name_created_dir = create_directory_save(config["path_save"])
+    with open("backup_files.json") as file:
+        conservation_objects = json.load(file)
+    copy_files(conservation_objects, full_name_created_dir)
 
 
-for key, value in list_configs.items():
-    backup_files(key, value)
-
-
-log_file.close()
+if __name__ == "__main__":
+    backup_files()
