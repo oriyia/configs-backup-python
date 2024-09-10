f5fec6368ec373ba46bfd47553025d3cb5d49541
Ilya Yarusov <originalyia@gmail.com>
2024-09-10 12:12:17 +0800

    add new config files

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 config = {
-    "path_save": Path().home() / Path("yandex/programming/saved_configs"),
+    "path_save": Path().home() / Path("yandex/information_technology/saved_configs"),
     "project_dir": Path(os.path.dirname(os.path.abspath(__file__))),
     "list_backup_files": Path(os.path.dirname(os.path.abspath(__file__))) / 'backup_files.json',
 }
